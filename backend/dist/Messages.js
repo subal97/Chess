97733911ace638cba2d6639e999cd3dd35a2410f
Subal Chatterjee <subalchatterjee9@gmail.com>
2024-10-27 19:11:33 +0530

    Added gitignore (#2)

--- backend/dist/Messages.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.REMATCH = exports.GAME_OVER = exports.MOVE = exports.INIT_GAME = void 0;
-exports.INIT_GAME = "INIT_GAME";
-exports.MOVE = "MOVE";
-exports.GAME_OVER = "GAME_OVER";
-exports.REMATCH = "REMATCH";
